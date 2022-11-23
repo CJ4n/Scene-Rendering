965a7c35dc73b97d37ac446dc393a7c472f113a3
Yan <jascichy@wp.pl>
2022-11-23 22:57:23 +0100

    UI modifications and more renames of folders and files

---
+++ Scene-Rendering-With-Raycasting/Scene-Rendering/ScanLine.cs
@@ -0,0 +1,90 @@
+﻿namespace SceneRendering
+{
+    public class ScanLine
+    {
+        private List<AETPointer> _AET;
+        private Stack<int> _sortedIdx;
+        private List<Point> _polygon;
+
+        public ScanLine(List<Point> polygon)
+        {
+            _AET = new List<AETPointer>();
+            this._polygon = polygon;
+
+            _sortedIdx = new Stack<int>(polygon.Select((point, idx) => new KeyValuePair<Point, int>(point, idx)).
+                OrderByDescending(pair => pair.Key.Y).
+                Select(pair => pair.Value));
+        }
+        public List<(List<int>, int)> GetIntersectionPoints()
+        {
+            List<(List<int> xList, int y)> outList = new List<(List<int>, int)>();
+            int yMin = (int)_polygon[_sortedIdx.Peek()].Y;
+            int yMax = (int)_polygon[_sortedIdx.Last()].Y;
+            var mockPointer = new AETPointer(0, 0, 0);
+
+            for (int y = yMin + 1; y <= yMax; ++y)
+            {
+                while (_sortedIdx.Count > 0 && _polygon[_sortedIdx.Peek()].Y == y - 1)
+                {
+                    var idx = _sortedIdx.Pop();
+                    var currentPoint = _polygon[idx];
+                    var prevPoint = _polygon[(idx - 1 + _polygon.Count) % _polygon.Count];
+                    if (prevPoint.Y > currentPoint.Y)
+                    {
+                        _AET.Add(new AETPointer(prevPoint.Y, currentPoint.X, Utils.Slope(currentPoint, prevPoint)));
+                    }
+                    else if (prevPoint.Y < currentPoint.Y)
+                    {
+                        mockPointer.x = prevPoint.X;
+                        mockPointer.yMax = (int)prevPoint.Y;
+                        _AET.Remove(mockPointer);
+                    }
+
+                    var nextPoint = _polygon[(idx + 1) % _polygon.Count];
+                    if (nextPoint.Y > currentPoint.Y)
+                    {
+                        _AET.Add(new AETPointer(nextPoint.Y, currentPoint.X, Utils.Slope(currentPoint, nextPoint)));
+                    }
+                    else if (nextPoint.Y < currentPoint.Y)
+                    {
+                        mockPointer.x = nextPoint.X;
+                        mockPointer.yMax = (int)nextPoint.Y;
+                        _AET.Remove(mockPointer);
+                    }
+                }
+
+                outList.Add((_AET.Select(ptr => ptr.X).OrderBy(x => x).ToList(), y));
+
+                _AET.RemoveAll((ptr) => ptr.yMax <= y);
+                foreach (var ptr in _AET)
+                {
+                    ptr.Update();
+                }
+            }
+            return outList;
+        }
+    }
+    public class AETPointer : IComparable<AETPointer>
+    {
+        public int yMax;
+        public double x;
+        private double _slope;
+
+        public AETPointer(double yMax, double x, double slope)
+        {
+            this.yMax = (int)yMax;
+            this.x = x;
+            this._slope = 1.0 / slope;
+        }
+        public int X { get => (int)Math.Round(x); }
+        public void Update()
+        {
+            x = _slope == Utils.Infinity ? Utils.Infinity : x + _slope;
+        }
+        public int CompareTo(AETPointer other)
+        {
+            var xCmp = x.CompareTo(other.x);
+            return xCmp == 0 ? yMax.CompareTo(other.yMax) : xCmp;
+        }
+    }
+}
---
+++ Scene-Rendering-With-Raycasting/Scene-Rendering/Utils.cs
@@ -0,0 +1,59 @@
+﻿using ObjLoader.Loader.Data;
+
+namespace SceneRendering
+{
+    public static class Utils
+    {
+        public const double Eps = 1e-8;
+        public const double Infinity = 1 / Eps;
+        public static double DotProduct(Vector3 v1, Vector3 v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+        }
+
+        public static double CosBetweenVersors(Vector3 v1, Vector3 v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+        }
+        public static double Magnitude(Vector3 v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+        }
+        public static void Normalize(Vector3 v)
+        {
+            var magnitude = Magnitude(v);
+            if (magnitude == 0)
+            {
+                return;
+            }
+            v.X /= magnitude;
+            v.Y /= magnitude;
+            v.Z /= magnitude;
+        }
+        public static bool AreTwoDoublesClose(double a, double b)
+        {
+            return Math.Abs(a - b) < Utils.Eps;
+        }
+        public static Vector3 CrossProduct(Vector3 v1, Vector3 v2)
+        {
+            double a1 = v1.X;
+            double a2 = v1.Y;
+            double a3 = v1.Z;
+            double b1 = v2.X;
+            double b2 = v2.Y;
+            double b3 = v2.Z;
+            Vector3 v = new Vector3(a2 * b3 - b2 * a3, a1 * b3 - b1 * a3, a1 * b2 - b1 * a2);
+            return v;
+        }
+        public static double Slope(PointF p1, PointF p2)
+        {
+            return Math.Abs(p1.X - p2.X) < Utils.Eps ? Utils.Infinity : (double)(p2.Y - p1.Y) / (double)(p2.X - p1.X);
+        }
+        public static double TrinagelArea(Vector3 v1, Vector3 v2, Vector3 p)
+        {
+            double Z = (v1.X - p.X) * (v2.Y - p.Y) - (v2.X - p.X) * (v1.Y - p.Y);
+            return Math.Sqrt(Z * Z) / 2;
+        }
+    }
+}
+
